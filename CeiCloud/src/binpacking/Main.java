863eb3fb30281b4e5d79fc2d5971e534158ad0b4
licarth <thomascarli@gmail.com>
2013-11-22 14:01:23 +0100

    thomas

--- CeiCloud/src/binpacking/Main.java
+++ CeiCloud/src/binpacking/Main.java
@@ -19,18 +19,18 @@ public class Main {
 //		pb.e = 9999999;
 //		pb.itemSizes = new int[]{2, 5, 10, 20, 25};
 		
-//		BPP bpp = new BPP(8, new int[]{8,8,7,5,4,3,3,2,2,1});
+		BPP bpp = new BPP(8, new int[]{8,8,2,5,4,3,3,2,2,1});
+		
+		NFD nfd = new NFD();
+		System.out.println(nfd.solve(bpp));
+		
+//		Set<BinType> binTypes = new HashSet<>();
+//		binTypes.add(new BinType(10, 10));
+//		binTypes.add(new BinType(100, 60));
+//		binTypes.add(new BinType(15, 12));
+//		VSCIFP p = new VSCIFP(binTypes, 10, 99999);
 //		
-//		NFD nfd = new NFD();
-//		System.out.println(nfd.solve(bpp));
-		
-		Set<BinType> binTypes = new HashSet<>();
-		binTypes.add(new BinType(10, 10));
-		binTypes.add(new BinType(100, 60));
-		binTypes.add(new BinType(15, 12));
-		VSCIFP p = new VSCIFP(binTypes, 10, 99999);
-		
-		CIFFD c = new CIFFD();
+//		CIFFD c = new CIFFD();
 		
 	}
 
